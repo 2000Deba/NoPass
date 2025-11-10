d8d4154410034bcabc8a2d75faa5c5d4f2e87882
2000Deba <deep2000seal@gmail.com>
2025-11-11 02:23:06 +0530

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
-module.exports = {
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "react/no-unescaped-entities": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "@next/next/no-img-element": "off",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    turbo: {
+      resolveAlias: {}, // This effectively disables turbopack
+    },
   },
 };
+
+module.exports = nextConfig;
